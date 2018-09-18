b39d73fb25d9d6a5df851584465abccdda6dfeb9
Darius Carter <dcarterbusinessemail@gmail.com>
2018-09-18 15:55:51 -0500

    Forgot .gitignore sorry !

---
+++ App.js
@@ -0,0 +1,27 @@
+const express = require ('express');
+const mongoose = require ('mongoose');
+const body = require ('body-parser');
+
+
+const port = process.env.PORT||5000;
+app = express()
+
+console.log('todo list Restful API started on :' + 5000);
+
+
+
+
+
+
+
+
+
+
+
+// Create an instance of the http server to handle HTTP requests
+//const newServer = http.createServer((req, res) => {
+//});
+
+
+//app.listen(5000, '127.0.0.1');
+//console.log('Node server running on port 5000');
